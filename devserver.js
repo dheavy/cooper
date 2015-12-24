a79faca0e32ac157ad1e40bf7f6824d874c0a017
Davy Peter Braun <hello@davybraun.com>
2015-12-24 12:47:00 +0100

    Add dev express server.

---
+++ devserver.js
@@ -0,0 +1,29 @@
+var path = require('path'),
+    express = require('express'),
+    webpack = require('webpack'),
+    config = require('./webpack.config'),
+    app = express(),
+    compiler = webpack(config);
+
+app.user(require('webpack-dev-middleware')(compiler, {
+  // noInfo: true,
+  stats: {
+    colors: true
+  },
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, 'localhost', function(err) {
+  if (err) {
+    return console.err(err);
+  }
+});
