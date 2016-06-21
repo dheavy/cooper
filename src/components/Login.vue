68109a1dcbeac79206ddcc9a342c6c422d3de00b
Davy Peter Braun <hello@davybraun.com>
2016-06-21 14:15:09 +0200

    Validation on Login form.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,57 +1,90 @@
 <template>
   <navigation :auth="false" :user-id="null"></navigation>
-  <div class="col-sm-4 col-sm-offset-4">
-    <h2>Log In</h2>
-    <div class="alert alert-danger" v-if="error">
-      <p>{{error}}</p>
-    </div>
-    <div class="form-group">
-      <input type="text" class="form-control" placeholder="Username" v-model="credentials.username">
-    </div>
-    <div class="form-group">
-      <input type="password" class="form-control" placeholder="Password" v-model="credentials.password">
-    </div>
-    <div class="form-group">
-      <button class="btn btn-primary" v-on:click="submit()">Log In</button>
-    </div>
-    <div class="form-group">
-    <a class="small" v-link="{path: '/password/reset'}">I forgot my username or password</a>
-    </div>
-  </div>
+  <validator name="loginValidation">
+    <form novalidate class="col-sm-4 col-sm-offset-4">
+      <h2>Log In</h2>
+
+      <div class="alert alert-danger" v-if="error">{{error}}</div>
+      <div class="alert alert-danger" v-if="submitted && $loginValidation.username.required">Username is required.</div>
+      <div class="alert alert-danger" v-if="submitted && $loginValidation.username.minlength">Username is too short.</div>
+      <div class="alert alert-danger" v-if="submitted && $loginValidation.password.required">Password is required.</div>
+      <div class="alert alert-danger" v-if="submitted && $loginValidation.password.minlength">Password is too short.</div>
+
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Username"
+          v-model="credentials.username"
+          v-validate:username="{required: true, minlength: 3}"
+          v-on:keyup="resetValidation()"
+        >
+      </div>
+
+      <div class="form-group">
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="credentials.password"
+          v-validate:password="{required: true, minlength: 6}"
+          v-on:keyup="resetValidation()"
+        >
+      </div>
+
+      <div class="form-group">
+        <button class="btn btn-primary" v-on:click.prevent="submit()">Log In</button>
+      </div>
+
+      <div class="form-group">
+        <a class="small" v-link="{path: '/password/reset'}">I forgot my username or password</a>
+      </div>
+    </form>
+  </validator>
 </template>
 
 <script>
 import navigation from './Navigation'
+import validator from 'vue-validator'
 import auth from '../services/auth'
-import store from '../store'
 
 export default {
   name: 'Login',
 
   data () {
     return {
-      store,
       credentials: {
         username: '',
         password: ''
       },
+      submitted: '',
       error: ''
     }
   },
 
   components: {
-    navigation
+    navigation, validator
   },
 
   methods: {
-    submit () {
-      this.error = ''
-      const credentials = {
-        username: this.credentials.username,
-        password: this.credentials.password
-      }
-      auth.login(this, credentials, '/my')
+    resetValidation () {
+      this.submitted = false
+      this.$resetValidation()
     },
+
+    submit () {
+      this.submitted = true
+      if (this.$loginValidation.valid) {
+        this.error = ''
+        const credentials = {
+          username: this.credentials.username,
+          password: this.credentials.password
+        }
+        auth.login(this, credentials, '/my')
+        this.resetValidation()
+      }
+    },
+
     parseError (err) {
       if (+err.status === 404) {
         this.error = 'Incorrect username or password.'
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './components/app'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import routeConfig from './route-config'
+import VueValidator from 'vue-validator'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
+Vue.use(VueValidator)
 
 export const router = new VueRouter({
   history: true,
