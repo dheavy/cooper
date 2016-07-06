45cd320d279fca4cac51e468547321598b2846f4
Davy Peter Braun <hello@davybraun.com>
2016-07-06 22:52:39 +0200

    Display racks of collection in currentuser profile.

---
+++ src/components/CollectionRack.vue
@@ -0,0 +1,175 @@
+<template>
+  <section class="collection">
+    <article v-for="n in 10 | orderBy 'created_at' -1">
+      <div class="{{$index === 0 ? 'cover' : 'thumb'}} {{collection.videos[$index] ? '' : 'empty'}} thumb-{{n}}">
+        <div v-if="collection.videos.length === 0 && $index === 0">
+          <p>There's nothing here... yet.<br>
+          <a href="#">Add a video</a></p>
+        </div>
+        <a href="#"><img v-if="collection.videos[$index]" v-bind:src="collection.videos[$index].poster" width="auto" height="100%"></a>
+      </div>
+    </article>
+    <article>
+      <a href="#" class="thumb thumb-more">more...</a>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Collection',
+
+  props: ['collection']
+}
+</script>
+
+<style lang="scss" scoped>
+.collection {
+  width: 100%;
+  height: 200px;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #CCCCCC;
+}
+
+.cover {
+  overflow: hidden;
+  width: 360px;
+  height: 180px;
+  float: left;
+  background-color: black;
+}
+
+.thumb {
+  overflow: hidden;
+  width: 140px;
+  height: 85px;
+  float: left;
+  background-color: black;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  display: none;
+}
+
+.thumb-more {
+  background-color: #CCCCCC;
+}
+
+.empty {
+  background-color: #CCCCCC;
+}
+
+@media screen and (min-width: 1200px) {
+  .thumb-0,
+  .thumb-1,
+  .thumb-2,
+  .thumb-3,
+  .thumb-4,
+  .thumb-5,
+  .thumb-6,
+  .thumb-7,
+  .thumb-8,
+  .thumb-9,
+  .thumb-more {
+    display: block;
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  .cover {
+    width: 200px;
+  }
+
+  .thumb {
+    width: 132px;
+  }
+
+  .thumb-0,
+  .thumb-1,
+  .thumb-2,
+  .thumb-3,
+  .thumb-4,
+  .thumb-5,
+  .thumb-6,
+  .thumb-7,
+  .thumb-8,
+  .thumb-9,
+  .thumb-more {
+    display: block;
+  }
+}
+
+@media screen and (max-width: 990px) {
+  .cover {
+    width: 264px;
+  }
+
+  .thumb-0,
+  .thumb-1,
+  .thumb-2,
+  .thumb-3,
+  .thumb-4,
+  .thumb-5,
+  .thumb-more {
+    display: block;
+  }
+
+  .thumb-6,
+  .thumb-7,
+  .thumb-8,
+  .thumb-9 {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 770px) {
+  .thumb {
+    width: 131px;
+  }
+
+  .thumb-0,
+  .thumb-1,
+  .thumb-2,
+  .thumb-3,
+  .thumb-more {
+    display: block;
+  }
+
+  .thumb-4,
+  .thumb-5,
+  .thumb-6,
+  .thumb-7,
+  .thumb-8,
+  .thumb-9 {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 575px) {
+  .thumb {
+    width: 130px;
+  }
+}
+
+@media screen and (max-width: 574px) {
+  .cover {
+    width: 100%;
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  .thumb-1,
+  .thumb-2,
+  .thumb-3,
+  .thumb-4,
+  .thumb-5,
+  .thumb-6,
+  .thumb-7,
+  .thumb-8,
+  .thumb-9 {
+    display: none;
+  }
+}
+</style>
--- src/components/Collections.vue
+++ src/components/Collections.vue
@@ -1,54 +1,67 @@
 <template>
   <section class="collections">
-    <div v-if="$loadingRouteData">Loading collections...</div>
-    <div v-if="!$loadingRouteData">
-      <div class="alert alert-danger" v-if="error">
-        <p>{{error}}</p>
-      </div>
-    </div>
+    <div v-if="loading"><p>Loading collections...</p></div>
+    <div v-if="error"><p>{{error}}</p></div>
+    <collection-rack
+      v-else
+      v-if="collections"
+      v-for="collection in collections | orderBy 'created_at' -1"
+      :collection="collection"
+    ></collection-rack>
   </section>
 </template>
 
 <script>
+import collectionRack from './CollectionRack'
 import {USERS_URL} from '../constants/api'
 import store from '../store'
 
 export default {
   name: 'Collections',
 
+  components: {
+    collectionRack
+  },
+
   data () {
     return {
       store,
-      error: ''
+      loading: false,
+      error: '',
+      collections: null
     }
   },
 
   route: {
     // Leave a small delay to display message during data acquisition.
-    data ({next}) {
-      setTimeout(() => {
-        next()
-      }, 1000)
+    data () {
+      this.loading = true
+      this.fetchCollections()
     }
   },
 
-  ready () {
-    this.fetchCollections(this)
-  },
-
   methods: {
-    fetchCollections: context => {
-      const id = context.id ? context.id : context.store.getUser().id
-      context.$http
+    fetchCollections () {
+      const id = this.id ? this.id : this.store.getUser().id
+      this.$http
         .get(`${USERS_URL}/${id}/collections`)
         .then(res => {
           console.log(res)
+          this.loading = false
+          this.collections = res.data
         })
         .catch(err => {
           console.log(err)
-          context.error = 'Oops... There was an error on our end. Please refresh this page.'
+          this.loading = false
+          this.error = 'Oops... There was an error on our end. Please refresh this page.'
         })
     }
   }
 }
 </script>
+
+<style lang="scss" scroped>
+.collections {
+  margin-top: 2em;
+}
+</style>
