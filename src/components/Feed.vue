8121e6b3a58641515b7a2738bebc5c776ae10cea
Davy Peter Braun <hello@davybraun.com>
2016-07-19 11:35:53 +0200

    Stub Feed component.

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,9 +1,41 @@
 <template>
-
+  <media :video="video" :playlist="[]" :scale="scales.normal" ></media>
 </template>
 
 <script>
+import {MEDIA_SCALE_NORMAL, MEDIA_SCALE_LARGE} from '../constants/config'
+import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
+import Waterfall from 'vue-waterfall/lib/waterfall'
+import Media from './media'
+
 export default {
-  name: 'Feed'
+  name: 'Feed',
+
+  components: {
+    Waterfall, WaterfallSlot, Media
+  },
+
+  data () {
+    return {
+      video: {
+        id: '16',
+        hash: '4322553893b442094406e1d5524fbdc2',
+        collection: '156',
+        originalUrl: 'http://www.dailymotion.com/video/x2ih1h4_fantastic-four-director-talks-johnny-storm-s-race-ign-news_',
+        embedUrl: 'https://www.dailymotion.com/embed/video/x2ih1h4',
+        duration: '00:01:11',
+        createdAt: '2016-07-06T08:32:40.789855Z',
+        isNaughty: 'false',
+        isPrivate: 'false',
+        poster: 'http://s2.dmcdn.net/JMwDE/x720-JQM.png',
+        title: 'Fantastic Four Director Talks Johnny Storm\'s Race - IGN News',
+        slug: 'fantastic-four-director-talks-johnny-storms-race-ign-news'
+      },
+      scales: {
+        normal: MEDIA_SCALE_NORMAL,
+        large: MEDIA_SCALE_LARGE
+      }
+    }
+  }
 }
 </script>
--- src/constants/config.js
+++ src/constants/config.js
@@ -2,3 +2,10 @@ export const FACEBOOK_APP_ID = '1096543940416567'
 export const FACEBOOK_OAUTH_REDIRECT_URL = 'http://localhost:8080/facebook'
 export const FACEBOOK_GRAPH_ME_URL = 'https://graph.facebook.com/me?fields=id,name,email&access_token='
 export const FACEBOOK_OAUTH_URL = `https://www.facebook.com/v2.6/dialog/oauth?client_id=${FACEBOOK_APP_ID}&redirect_uri=${FACEBOOK_OAUTH_REDIRECT_URL}&scope=public_profile,email&response_type=token`
+
+export const MEDIA_NORMAL_WIDTH = 273
+export const MEDIA_NORMAL_HEIGHT = 204
+export const MEDIA_LARGE_WIDTH = MEDIA_NORMAL_WIDTH * 2
+export const MEDIA_LARGE_HEIGHT = MEDIA_NORMAL_HEIGHT * 2
+export const MEDIA_SCALE_NORMAL = 'normal'
+export const MEDIA_SCALE_LARGE = 'large'
