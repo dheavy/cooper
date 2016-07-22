1285341d1c5ad87d5b92abf01917ca8ae40e0824
Davy Peter Braun <hello@davybraun.com>
2016-07-22 16:31:03 +0200

    Comment filters.

--- src/services/filters.js
+++ src/services/filters.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import moment from 'moment'
 
 const filters = () => {
+  // Format date display.
   Vue.filter('date', value => {
     return moment(value).format('LL')
   })
 
+  // Replace the end of a long text by an ellipsis ("...").
   Vue.filter('ellipsis', (value, length, dots = '...') => {
     if (value.length > length) {
       return value.slice(0, length - dots.length) + dots
