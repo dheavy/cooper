a0f02f9649de79758f0f413ab400c2d62bfdf79c
Davy Peter Braun <hello@davybraun.com>
2016-08-05 16:02:53 +0200

    Fix mixin.

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,12 +1,14 @@
 import errors from '../services/errors'
 
 export const parseError = {
-  parseError (err) {
-    // console.log(err)
-    if (errors[err.message]) {
-      this.error = errors[err.message]
-    } else {
-      this.error = 'Oops... something went wrong, please try again.'
+  methods: {
+    parseError (err) {
+      // console.log(err)
+      if (errors[err.message]) {
+        this.error = errors[err.message]
+      } else {
+        this.error = 'Oops... something went wrong, please try again.'
+      }
     }
   }
 }
