0aa95542826cbc7d86849b81734f88780f0353da
Davy Peter Braun <hello@davybraun.com>
2016-08-25 12:04:03 +0200

    Stub modal for Player.

---
+++ src/components/ModalPlayer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div name="close" class="modal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ModalPlayer'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
