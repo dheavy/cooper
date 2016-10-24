46c76c0e5cc3633268ddf989e2fb493e70089f88
Davy Peter Braun <hello@davybraun.com>
2016-10-24 18:08:57 +0200

    Create and place FooterComponent.

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="footer-component">
+    <footer class="small col-sm-4 offset-sm-4">
+      <a href="#">About</a> • <a href="#">Legal notice</a> • <a href="#">Contact</a>
+    </footer>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'FooterComponent'
+  }
+</script>
+
+<style lang="scss" scoped>
+  footer {
+    text-align: center;
+    color: white;
+
+    a, a:visited, a:active, a:hover {
+      text-decoration: none;
+      color: white;
+
+      margin: 0 1rem;
+    }
+  }
+</style>
